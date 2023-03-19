d56dee3874eb303b35747013f7918761d93c4f09
aabirbasu <aabirbasu906@gmail.com>
2023-03-18 21:54:00 -0230

    added walmart search functionality to onlineSearch

--- App_Code/onlineSearch.java
+++ App_Code/onlineSearch.java
@@ -8,12 +8,26 @@ private static void searchItem(String Item){
 	WebDriver driver = new ChromeDriver();
 	
 	//search on walmart website
-	String search = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
-	driver.get(search);
-	WebElement firstmatch = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
-	String walmartItem = firstmatch.getText();
+	String search0 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
+	driver.get(search0);
+	String walmartItem = driver.findElement(By.className("css-1p4va6y eudvd6x0")).getText();
 	
-	//search on sobeys website
+	//search on wholesaleClub website
+	String search1 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
+	driver.get(search1);
+	WebElement firstmatch1 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
+	String sobeysItem = firstmatch1.getText();
 	
+	//search on dollarama website
+	String search2 = String.format("https://www.dollarama.com/en-CA/Search?keywords=%s",Item.replace(' ', '+'));
+	driver.get(search2);
+	WebElement firstmatch2 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
+	String dollaramaItem = firstmatch2.getText();
+	
+	//search on costco website
+	String search3 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
+	driver.get(search3);
+	WebElement firstmatch3 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
+	String costcoItem = firstmatch3.getText();
 }
 }
