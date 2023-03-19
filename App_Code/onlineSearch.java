015e4e592a36c19fa20db6b26c0e9da7446e0f39
aabirbasu <aabirbasu906@gmail.com>
2023-03-18 22:50:46 -0230

    added wholesaleClub, dollarama

--- App_Code/onlineSearch.java
+++ App_Code/onlineSearch.java
@@ -3,31 +3,84 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 /*Focuses on online searching*/
 
-public class onlineSearch {
-private static void searchItem(String Item){
+interface onlineSearch {
+private static void searchItem();
 	WebDriver driver = new ChromeDriver();
-	
-	//search on walmart website
-	String search0 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
-	driver.get(search0);
-	String walmartItem = driver.findElement(By.className("css-1p4va6y eudvd6x0")).getText();
-	
-	//search on wholesaleClub website
-	String search1 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
-	driver.get(search1);
-	WebElement firstmatch1 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
-	String sobeysItem = firstmatch1.getText();
-	
-	//search on dollarama website
-	String search2 = String.format("https://www.dollarama.com/en-CA/Search?keywords=%s",Item.replace(' ', '+'));
-	driver.get(search2);
-	WebElement firstmatch2 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
-	String dollaramaItem = firstmatch2.getText();
-	
-	//search on costco website
-	String search3 = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
-	driver.get(search3);
-	WebElement firstmatch3 = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
-	String costcoItem = firstmatch3.getText();
+	String search;
 }
+	
+public class Walmart implements onlineSearch{
+	Walmart(){
+		String walmartItem;
+		String walmartPrice;
+	}
+	private static void searchItem(String Item) {
+		
+		//search on walmart website
+		search = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
+		driver.get(search);
+		try {
+		walmartItem = driver.findElement(By.className("css-1p4va6y eudvd6x0")).getText();
+		walmartPrice = driver.findElement(By.className("css-2vqe5n esdkp3p0")).getText();
+		}
+		catch{
+			walmartItem = "Item not found";
+			walmartPrice = "";
+		}
+	}
 }
+	
+public class WholesaleClub implements onlineSearch{
+	WholesaleClub(){
+		String wholesaleClubPrice;
+		String wholesaleClubItem;
+	}
+	private static void searchItem() {
+		
+		//search on wholesaleClub website
+		search = String.format("https://www.wholesaleclub.ca/search?search-bar=%s",Item.replace(' ', '%20'));
+	    driver.get(search);
+	    try {
+		    wholesaleClubPrice = driver.findElement(By.className("price__value selling-price-list__item__price selling-price-list__item__price--now-price__value")).getText();
+		    wholesaleClubItem = driver.findElement(By.className("product-name__item product-name__item--brand")).getText() + driver.findElement(By.className("product-name__item product-name__item--name")).getText();
+	    }
+	    catch{
+	    	wholesaleClubPrice = "";
+	    	wholesaleClubItem = "Item not found";
+	    }
+	}
+}
+	
+public class Dollarama implements onlineSearch{
+	private static void searchItem() {
+		
+		//search on dollarama website
+		search = String.format("https://www.dollarama.com/en-CA/Search?keywords=%s",Item.replace(' ', '+'));
+		driver.get(search);
+		try {
+		String dollaramaItem = driver.findElement(By.className("js-display-name")).getText();
+		String dollaramaPrice = driver.findElement(By.className("product-tile-price")).getText();
+		}
+		catch{
+		String dollaramaItem = "Item not found";
+		String dollaramaPrice = "";
+		}
+	}
+}
+	
+public class Costco implements onlineSearch {
+	Costco(){
+		String costcoItem;
+		String costcoPrice;
+	}
+	private static void searchItem() {
+		
+		//search on costco website
+		search = String.format("https://www.walmart.ca/search?q=%s",Item.replace(' ', '+'));
+		driver.get(search);
+		String costcoItem = driver.findElement(By.className("css-1p4va6y eudvd6x0"));
+		
+	}
+}
+	
+
